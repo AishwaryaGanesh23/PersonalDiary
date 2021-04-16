a61e16b6c7b09cbe5817b3eb2f1279a3c6bfc118
AishwaryaGanesh23 <you@example.commca.1901.unigoa.com>
2021-04-16 20:00:59 +0530

    laravel files-backend

---
+++ Laravel Backend/database/migrations/2021_04_09_084725_create_diaryentry_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDiaryentryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diary_entries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('entrycontent');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
